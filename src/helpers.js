b5337b0f45276a04cbe3792f780fa1aaa0f2626c
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-02-18 11:36:09 -0800

    Fixed bug in helpers.

--- src/helpers.js
+++ src/helpers.js
@@ -1,9 +1,9 @@
 import merge from 'lodash/merge';
 
 export const $ = (...styles) => ({
-    style: (merge(...styles)),
+    style: merge({}, ...styles),
 });
 
 export const _ = (...attrs) => ({
-    attrs: merge(...attrs),
+    attrs: merge({}, ...attrs),
 });
