f7a3dc508b361e9fc5adda7f5ea36cce2f338240
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-02-18 11:48:05 -0800

    Finished the styling for a card.

--- src/view.js
+++ src/view.js
@@ -1,21 +1,35 @@
-import { div, a, img, b } from '@cycle/dom';
+import { div, a, img, sub } from '@cycle/dom';
 import merge from 'lodash/merge';
 import { $, _ } from './helpers';
-import { overflowAuto, relative, mt2, z2, roboto, whiteText, bgGrey800, br1, fw5 } from './styles';
+import {
+    overflowAuto, relative, mt2, z2, roboto, whiteText, w2,
+    bgGrey800, br1, fw5, h15, pa3, pr4, fl, w25, vTop, h2,
+    greyText, absolute, bottom05, right1, mAuto, pl2, vMid,
+} from './styles';
 
-const width = { width: 320 };
+const cardSize = { width: 320, height: 120 };
 
 export default state$ => ({
     DOM: state$.map(post =>
-        div($(width, relative, mt2, z2, roboto, whiteText, bgGrey800, br1), [
-            a(merge(_({ href: post.url }), $(roboto, fw5)), [
-                div($(overflowAuto), [
-                    img(_({ src: post.image })),
-                    div([
-                        b(post.title),
+        div($(mAuto), [
+            div($(cardSize, relative, mt2, z2, roboto, whiteText, bgGrey800, br1), [
+                a(merge(_({ href: post.url }), $(roboto, fw5)), [
+                    div($(overflowAuto), [
+                        img(merge(_({ src: post.image }), $(pa3, pr4, fl, w25))),
+                        div($(vTop, pa3), [
+                            post.title,
+                            div($(h15), [
+                                sub($(greyText, absolute, bottom05, right1), [
+                                    a(_({ href: post.source.url }), [
+                                        `${post.added} • ${post.source.name} `,
+                                        img(merge(_({ href: post.image }), $(pl2, vMid, w2, h2))),
+                                    ]),
+                                ]),
+                            ]),
+                        ]),
                     ]),
                 ]),
             ]),
-        ])
+        ]),
     ),
 });
